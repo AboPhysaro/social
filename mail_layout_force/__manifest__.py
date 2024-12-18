86a4e9e7c4c8d39aebaed1e1e8ca22272480a744
VicGuillaume <vigu@odoo.com>
2024-12-18 11:22:48 +0100

    [UPG][4384703] mail_layout_force: Upgrade module

--- mail_layout_force/__manifest__.py
+++ mail_layout_force/__manifest__.py
@@ -5,13 +5,14 @@
 {
     "name": "Mail Layout Force",
     "summary": "Force a mail layout on selected email templates",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "maintainers": ["ivantodorovich"],
-    "website": "https://github.com/OCA/social",
+    "website": "https://github.com/AboPhysaro/social/tree/17.0/mail_layout_force",
     "license": "AGPL-3",
     "category": "Marketing",
     "depends": ["mail"],
     "demo": ["demo/mail_layout.xml"],
     "data": ["data/mail_layout.xml", "views/mail_template.xml"],
+    "application": True,
 }
